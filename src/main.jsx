eff5321a7651f82a1d1426fe3bc5ed3dfe5b3a2f
alire1992 <alirezasa24@gmail.com>
2024-05-04 18:53:04 +0330

    Fetching data from ombdi server

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./components/StarRating";
-// import App from './App.jsx'
-// import './index.css'
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating defaultRating={2} />
-    <StarRating maxRating={5} color="red" message={["bad", "normal", "good"]} />
+    <App />
   </React.StrictMode>
 );
