70a8c2b39f0c669ba78f18a23ca069e3a524c7bc
alire1992 <alirezasa24@gmail.com>
2024-05-19 19:37:53 +0330

    Focus and clean search input by enter when input search not foused

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,32 @@
 /* eslint-disable react/prop-types */
 
+import { useEffect, useRef } from "react";
+
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    inputEl.current.focus();
+    function callback(e) {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+
+    return () => document.removeEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
       type="text"
       placeholder="Search movies..."
+      ref={inputEl}
       value={query}
       onChange={(e) => setQuery(e.target.value)}
     />
